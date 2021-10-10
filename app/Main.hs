e09730b623cdc5aaef6fb4cc74e5015fc9a9123c
cutsea110 <cutsea110@gmail.com>
2021-10-10 09:59:53 +0900

    modify

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,21 @@ import qualified Data.Vector.Mutable as MV
 
 main :: IO ()
 main = do
+  putStrLn "-----------------"
+  putStrLn " Generating"
+  putStrLn "-----------------"
   genVectors
+  
+  putStrLn "-----------------"
+  putStrLn " Modifing"
+  putStrLn "-----------------"
+  modVectors
 
 --------------------------------------
 -- Generating Vectors
 --------------------------------------
 
 genVectors = do
-  putStrLn "-----------------"
-  putStrLn " Generating"
-  putStrLn "-----------------"
-  
   let x = V.fromList [10, 20, 30, 40]
   putStrLn $ "V.fromList [10, 20, 30, 40] -> " ++ show x
 
@@ -49,3 +53,31 @@ genVectors = do
 
   let x = V.enumFromTo 0 5 V.++ V.enumFromTo 6 10
   putStrLn $ "V.enumFromTo 0 5 V.++ V.enumFromTo 6 10 -> " ++ show x
+
+  let x = V.iterateN 10 id 1
+  putStrLn $ "V.iterateN 10 id 1 -> " ++ show x
+
+  let x = V.iterateN 10 (*2) 1
+  putStrLn $ "V.iterateN 10 (*2) 1 -> " ++ show x
+
+modVectors = do
+  let x = V.map (^2) (V.enumFromTo 0 10)
+  putStrLn $ "V.map (^2) (V.enumFromTo 0 10) -> " ++ show x
+
+  let x = V.filter odd $ V.enumFromTo 0 10
+  putStrLn $ "V.filter odd (V.enumFromTo 0 10) -> " ++ show x
+
+  let x = V.foldl (+) 0 $ V.enumFromTo 0 10
+  putStrLn $ "V.foldl (+) 0 $ V.enumFromTo 0 10 -> " ++ show x
+  
+  let x = V.foldl (*) 1 $ V.enumFromTo 1 10
+  putStrLn $ "V.foldl (*) 1 $ V.enumFromTo 1 10 -> " ++ show x
+  
+  let x = V.scanl (+) 0 $ V.enumFromTo 0 10
+  putStrLn $ "V.scanl (+) 0 $ V.enumFromTo 0 10 -> " ++ show x
+
+  let x = V.scanl (*) 1 $ V.enumFromTo 1 10
+  putStrLn $ "V.scanl (*) 1 $ V.enumFromTo 1 10 -> " ++ show x
+
+  let x = V.zip (V.enumFromTo 1 10) (V.enumFromTo 1 10)
+  putStrLn $ "V.zip (V.enumFromTo 1 10) (V.enumFromTo 1 10) -> " ++ show x
