3921f7ee2df4a8b7ccce2265009e0853e5b85089
cutsea110 <cutsea110@gmail.com>
2021-10-10 09:15:31 +0900

    generating

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,51 @@
 module Main where
 
 import Control.Monad.ST
-import Data.Vector
-import Data.Vector.Mutable
-
-vecHandle :: IO (Vector Int)
-vecHandle = do
-    v <- new 1
-    write v 0 (1::Int)
-    freeze v
-
-vecHandle2 :: ST s (Vector Int)
-vecHandle2 = do
-    v <- new 1
-    write v 0 (1::Int)
-    freeze v
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
 
 main :: IO ()
 main = do
-  print =<< vecHandle
-  print (runST vecHandle2)
+  genVectors
+
+--------------------------------------
+-- Generating Vectors
+--------------------------------------
+
+genVectors = do
+  putStrLn "-----------------"
+  putStrLn " Generating"
+  putStrLn "-----------------"
+  
+  let x = V.fromList [10, 20, 30, 40]
+  putStrLn $ "V.fromList [10, 20, 30, 40] -> " ++ show x
+
+  let x = V.enumFromStepN 10 10 4
+  putStrLn $ "V.enumFromStepN 10 10 4 -> " ++ show x
+
+  let x = V.enumFromN 10 4
+  putStrLn $ "V.enumFromN 10 4 -> " ++ show x
+
+  let x = V.empty :: V.Vector Int
+  putStrLn $ "V.empty -> " ++ show x
+
+  let x = V.singleton 2
+  putStrLn $ "V.singleton 2 -> " ++ show x
+
+  let x = V.replicate 10 2
+  putStrLn $ "V.replicate 10 2 -> " ++ show x
+
+  let x = V.generate 10 id
+  putStrLn $ "V.generate 10 id -> " ++ show x
+
+  let x = V.generate 10 (^2)
+  putStrLn $ "V.generate 10 (^2) -> " ++ show x
+
+  let x = V.generate 5 (V.replicate 3)
+  putStrLn $ "V.generate 5 (V.replicate 3) -> " ++ show x
+
+  let x = V.take 3 $ V.enumFromTo 0 11
+  putStrLn $ "V.take 3 $ V.enumFromTo 0 11 -> " ++ show x
+
+  let x = V.enumFromTo 0 5 V.++ V.enumFromTo 6 10
+  putStrLn $ "V.enumFromTo 0 5 V.++ V.enumFromTo 6 10 -> " ++ show x
